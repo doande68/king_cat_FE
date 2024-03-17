f2b3e1949fc6f770eaf8001da6e9e152f9cccc2a
GinzaTech <thanhloc150204@gmail.com>
2024-03-17 12:17:38 +0700

    update html

--- home/admin.js
+++ home/admin.js
@@ -1,6 +1,6 @@
 document.addEventListener('keydown', function(event) {
 
-  if (event.ctrlKey && event.shiftKey && event.key === 'A') {
+  if (event.ctrlKey && event.shiftKey && event.key === 'G') {
 
     window.location.href = '../admin/home/home.html';
   }
